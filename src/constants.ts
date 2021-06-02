09b2bd17c1f959f356a7ee7dd7a8314c73134c2e
Agin-DropDisco <76779267+Agin-DropDisco@users.noreply.github.com>
2021-06-02 19:34:34 +0700

    just test before move to the 0.1.5

---
+++ src/constants.ts
@@ -0,0 +1,113 @@
+import JSBI from 'jsbi'
+import PERMISSIVE_MULTICALL_ABI from './abis/PermissiveMulticall.json'
+import STAKING_REWARDS_FACTORY_ABI from './abis/staking-rewards-distribution-factory.json'
+import STAKING_REWARDS_DISTRIBUTION_ABI from './abis/staking-rewards-distribution.json'
+import {
+  rinkeby as coreRinkeby,
+  mainnet as coreMainnet,
+  matic as coreMatic,
+  xdai as coreXDai
+} from './abis/contracts.json'
+import {
+  mainnet as peripheryMainnet,
+  rinkeby as peripheryRinkeby,
+  matic as peripheryMatic,
+  xdai as peripheryXDai
+} from './abis/router.json'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 1,
+  RINKEBY = 4,
+  MATIC = 80001,
+  XDAI = 100
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export const FACTORY_ADDRESS: { [chainId: number]: string } = {
+  [ChainId.MAINNET]: coreMainnet.factory,
+  [ChainId.RINKEBY]: coreRinkeby.factory,
+  [ChainId.MATIC]: coreMatic.factory,
+  [ChainId.XDAI]: coreXDai.factory
+}
+
+export const ROUTER_ADDRESS: { [chainId in ChainId]?: string } = {
+  [ChainId.RINKEBY]: peripheryRinkeby.router,
+  [ChainId.MAINNET]: peripheryMainnet.router,
+  [ChainId.MATIC]: peripheryMatic.router,
+  [ChainId.XDAI]: peripheryXDai.router
+}
+
+export const STAKING_REWARDS_FACTORY_ADDRESS: { [chainId: number]: string } = {
+  [ChainId.MAINNET]: '0x0000000000000000000000000000000000001234',
+  [ChainId.RINKEBY]: '0x7929240c367f9b2638D652DBB3D38f7AC516D71B',
+  [ChainId.MATIC]: '0x680242DeD6b8Fc56e5363e67873B983BE78b2166',
+  [ChainId.XDAI]: '0x0000000000000000000000000000000000001234'
+}
+
+export const DXSWAP_TOKEN_LIST_ID: { [chainId: number]: number } = {
+  [ChainId.MAINNET]: 1,
+  [ChainId.RINKEBY]: 1,
+  [ChainId.MATIC]: 1,
+  [ChainId.XDAI]: 5
+}
+
+
+export const INIT_CODE_HASH = '0x21d289536920f920d222409e32789ea13419f64176abb684929511dc5fefc20f'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _25 = JSBI.BigInt(25)
+export const SECONDS_IN_YEAR = JSBI.BigInt(31536000)
+export const _30 = JSBI.BigInt(30)
+export const _100 = JSBI.BigInt(100)
+export const _1000 = JSBI.BigInt(1000)
+export const _10000 = JSBI.BigInt(10000)
+
+export const defaultSwapFee = _25
+export const defaultProtocolFeeDenominator = FIVE
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
+
+const PERMISSIVE_MULTICALL_ADDRESS: { [chainId: number]: string } = {
+  [ChainId.MAINNET]: '0x0946f567d0ed891e6566c1da8e5093517f43571d',
+  [ChainId.RINKEBY]: '0xcE7D0874A4EF89F38E06A3376026e1cb34D409Da',
+  [ChainId.MATIC]: '0x260B719917D3b33040ca431a790464CA22033550',
+  [ChainId.XDAI]: '0x4E75068ED2338fCa56631E740B0723A6dbc1d5CD'
+}
+
+export {
+  PERMISSIVE_MULTICALL_ABI,
+  PERMISSIVE_MULTICALL_ADDRESS,
+  STAKING_REWARDS_FACTORY_ABI,
+  STAKING_REWARDS_DISTRIBUTION_ABI
+}
